5d18797ed919a437e0c52552e3b755391fc8903d
Todd Langford-Archer <toddpla@gmail.com>
2019-07-07 18:40:52 +0100

    Add deploy commands

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,16 +1,11 @@
 import React from 'react';
-
 import Players from './Players';
-import Games from './PlayerGames'
-
 
 import "../styles/style.scss";
-
 
 class Leaderboard extends React.Component {
 
     render() {
-
         return (
             <div>
                 <div className="container">
